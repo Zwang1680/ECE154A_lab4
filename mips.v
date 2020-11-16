8105dfb656a036bbe9df56634169b564abd02933
Zion Wang <zion@ucsb.umail.edu>
2020-11-16 14:38:35 -0800

    coverted to from sys to verilog

--- mips.v
+++ mips.v
@@ -33,8 +33,13 @@ module controller(input   [5:0] op, funct,
                   output        regdst, regwrite,
                   output        jump,
                   output  [2:0] alucontrol);
+wire [1:0] aluop;
+wire branch;
+maindec md(op, memtoreg, memwrite, branch,
+alusrc, regdst, regwrite, jump, aluop);
+aludec ad(funct, aluop, alucontrol);
+assign pcsrc = branch & zero;
 
-// **PUT YOUR CODE HERE**
 
 endmodule
 
@@ -50,8 +55,28 @@ module datapath(input          clk, reset,
                 input   [31:0] instr,
                 output  [31:0] aluout, writedata,
                 input   [31:0] readdata);
-
-// **PUT YOUR CODE HERE**                
-                
+wire [4:0] writereg;
+wire [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
+wire [31:0] signimm, signimmsh;
+wire [31:0] srca, srcb;
+wire [31:0] result;
+// next PC logic
+flopr #(32) pcreg(clk, reset, pcnext, pc);
+adder pcadd1(pc, 32'b100, pcplus4);
+sl2 immsh(signimm, signimmsh);
+adder pcadd2(pcplus4, signimmsh, pcbranch);
+mux2 #(32) pcbrmux(pcplus4, pcbranch, pcsrc, pcnextbr);
+mux2 #(32) pcmux(pcnextbr, {pcplus4[31:28],
+instr[25:0], 2'b00}, jump, pcnext);
+// register file logic
+regfile rf(clk, regwrite, instr[25:21], instr[20:16],
+writereg, result, srca, writedata);
+mux2 #(5) wrmux(instr[20:16], instr[15:11],
+regdst, writereg);
+mux2 #(32) resmux(aluout, readdata, memtoreg, result);
+signext se(instr[15:0], signimm);
+// ALU logic
+mux2 #(32) srcbmux(writedata, signimm, alusrc, srcb);
+alu alu(srca, srcb, alucontrol, aluout, zero);              
 endmodule
 
