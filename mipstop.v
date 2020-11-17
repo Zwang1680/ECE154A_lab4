5e3f7b7f81facd97384b79f156a08a8bae5d0b76
Zion Wang <zion@ucsb.umail.edu>
2020-11-16 17:32:06 -0800

    stuff

--- mipstop.v
+++ mipstop.v
@@ -1,8 +1,8 @@
 // Top level system including MIPS and memories
 
 module top(input clk, reset,
-output logic [31:0] writedata, dataadr,
-output logic memwrite);
+output  [31:0] writedata, dataadr,
+output  memwrite);
 
   wire [31:0] pc, instr, readdata;
   
