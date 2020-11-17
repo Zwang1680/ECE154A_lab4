46b583393598ec8bfe10a9e0f258a33d3e4bbd04
Zion Wang <zion@ucsb.umail.edu>
2020-11-16 16:55:55 -0800

    fixed small bug

--- mips.v
+++ mips.v
@@ -94,8 +94,8 @@ reg [31:0] rf[31:0];
 // on falling edge of clk
 always @(posedge clk)
 if (we3) rf[wa3] <= wd3;
-assign rd1 = (ra1 ! = 0) ? rf[ra1] : 0;
-assign rd2 = (ra2 ! = 0) ? rf[ra2] : 0;
+assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
+assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
 endmodule
 
 module maindec(input  [5:0] op,
